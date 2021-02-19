fb30cbf20d86c540112046fe43e1adc44e3dccc7
Caudill-Z <caudill.z@yahoo.com>
2021-02-19 13:39:32 -0600

    Changed get method to serve an HTML file

--- myApp.js
+++ myApp.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 
     app.get('/', (req, res) => {
-        res.send('Hello Express');
+        res.sendFile(__dirname + '/views/index.html')
     })
+
+    //Serve string
+    // app.get('/', (req, res) => {
+    //     res.send('Hello Express');
+    // })
 
  module.exports = app;
  
