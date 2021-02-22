7ff26064930dc2e58ac9f0685194cdbdef1c97bc
Caudill-Z <caudill.z@yahoo.com>
2021-02-22 14:43:21 -0600

    added stylesheet to node

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+    //Serve css
+    app.use('/public', express.static(__dirname + '/public'))
+
+    //Serve html
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/views/index.html')
     })
